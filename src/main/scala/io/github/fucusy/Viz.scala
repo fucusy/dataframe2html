f9f09e3dd929390dc95004eca3b72fdec2b84ac7
chenqiang <fucus@qq.com>
2020-04-17 13:33:22 +0800

    make it work when having img url

--- src/main/scala/io/github/fucusy/Viz.scala
+++ src/main/scala/io/github/fucusy/Viz.scala
@@ -4,9 +4,18 @@ object Viz {
 
   def data2html(column2data: Seq[(String, Seq[String])], imgCols: Seq[String], title: String): String = {
     ""
-    val tableContent = column2data.map{
+    val tableContent = column2data.map {
       case (colName: String, data: Seq[String]) =>
-        val dataHtml = data.map(element => s"<td>$element</td>").mkString("")
+        val dataHtml = data
+          .map {
+            element =>
+              if (imgCols.contains(colName)) {
+                s"""<img src="$element"/>"""
+              } else {
+                element
+              }
+          }
+          .map(element => s"<td>$element</td>").mkString("")
         s"<tr><th>$colName</th>$dataHtml</tr>"
     }.mkString("")
     s"""
