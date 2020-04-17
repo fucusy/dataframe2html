6786140f3ec0b837477f278562a6ef8e1543949e
chenqiang <fucus@qq.com>
2020-04-17 11:38:13 +0800

    add first core function, data2html

---
+++ src/test/scala/io/github/fucusy/TestViz.scala
@@ -0,0 +1,16 @@
+package io.github.fucusy
+
+import org.scalatest.funsuite.AnyFunSuite
+
+import scala.io.Source
+
+class TestViz extends AnyFunSuite {
+  test("Test data2html") {
+    val c2d = Seq(("name", Seq("bob", "amy")), ("hobby", Seq("dance", "swimming")))
+    val title = "Users"
+    val html = Viz.data2html(c2d, title)
+    val trueHtml = Source.fromFile("src/test/resources/data2html.html").mkString("")
+    assert(html.replaceAll("\\s", "") == trueHtml.replaceAll("\\s", "")
+    )
+  }
+}
